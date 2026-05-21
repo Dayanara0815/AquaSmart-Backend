8df45034bf458100f81f134ad7e7b2885bfd7fdb
Dayanara <dayanaracarorojas@gmail.com>
2026-05-20 20:48:24 -0500

    Estructura inicial del proyecto

---
+++ AquaSmart/src/main/java/com/AquaSmart/AquaSmartApplication.java
@@ -0,0 +1,13 @@
+package com.AquaSmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaSmartApplication.class, args);
+	}
+
+}
